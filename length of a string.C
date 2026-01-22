4230c43030c88333ec37af59f871a556e2d574b6
ruthrasri827-netizen <ruthrasri827@gmail.com>
2026-01-22 21:33:39 +0530

    Create length of a string.C

---
+++ length of a string.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = '\0';
+
+    printf("Length of the string is: %d\n", strlen(str));
+
+    return 0;
+}

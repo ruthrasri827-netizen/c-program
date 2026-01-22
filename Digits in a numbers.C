492dd39cbde067805f24aab19d065d72a1cc7962
ruthrasri827-netizen <ruthrasri827@gmail.com>
2026-01-22 14:14:23 +0530

    Create Digits in a numbers.C

---
+++ Digits in a numbers.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num == 0)
+        count = 1;
+
+    while (num != 0) {
+        count++;
+        num = num / 10;
+    }
+
+    printf("Number of digits = %d", count);
+
+    return 0;
+}
